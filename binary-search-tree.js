1763f190debbf95708f7a06209cea9c424609387
Forkentiney <jseverud@proton.me>
2026-07-01 09:50:00 -0500

    I'm stupid. Tree() was not returning anything
    Factory functions... I should be able to do this with my eyes closed at
    this point

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,19 +1,29 @@
-function node(value = null, left = null, right = null) {};
+function node(data, left = null, right = null) {
+	return { data: data, left: left, right: right };
+};
 
-function binarySearchTree(array, left, right) {
+function tree(array) {
+	const root = buildTree(array);
+}
+
+function buildTree(array, left, right) {
 	if (left > right) return null;
 
 	let mid = left + Math.floor((right - left) / 2);
 	let root = node(array[mid]);
 
-	root.left = binarySearchTree(array, left, mid - 1);
-	root.right = binarySearchTree(array, mid + 1, right);
+	root.left = buildTree(array, left, mid - 1);
+	root.right = buildTree(array, mid + 1, right);
 
 	return root;
 };
 
 function displayBST(array) {
-	return binarySearchTree(array, 0, array.length - 1);
+	return buildTree(array, 0, array.length - 1);
 };
 
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const test = displayBST(array);
+
+console.log(test);
