acfcc46424303898535d34e6539b071ca7bec5b4
Forkentiney <jseverud@proton.me>
2026-07-01 12:12:43 -0500

    prettyPrint now working as intended.

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -3,7 +3,8 @@ function node(data, left = null, right = null) {
 };
 
 function tree(array) {
-	const root = buildTree(array);
+	const root = buildTree(array, 0, array.length - 1);
+	return { root };
 }
 
 function buildTree(array, left, right) {
@@ -18,12 +19,20 @@ function buildTree(array, left, right) {
 	return root;
 };
 
-function displayBST(array) {
-	return buildTree(array, 0, array.length - 1);
-};
-
+// Test code
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-
-const test = displayBST(array);
+const test = tree(array);
 
 console.log(test);
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+prettyPrint(test.root);
